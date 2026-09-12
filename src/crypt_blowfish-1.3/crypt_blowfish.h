31fe23a7720cf580cc29dee96792c716eea4d7fd
Avelanda <gorthell@gmail.com>
2026-09-13 01:15:55 +0200

    Update crypt_blowfish.h
    Modularizing system's core functions for optimized mechanisms.

--- src/crypt_blowfish-1.3/crypt_blowfish.h
+++ src/crypt_blowfish-1.3/crypt_blowfish.h
@@ -1,5 +1,7 @@
 /*
  * Written by Solar Designer <solar at openwall.com> in 2000-2011.
+ * Copyright © 2026 |Avelanda|
+ * All rights reserved.
  * No copyright is claimed, and the software is hereby placed in the public
  * domain.  In case this attempt to disclaim copyright and place the software
  * in the public domain is deemed null and void, then the software is
@@ -14,14 +16,39 @@
  * See crypt_blowfish.c for more information.
  */
 
+#include <iostream>
+
 #ifndef _CRYPT_BLOWFISH_H
 #define _CRYPT_BLOWFISH_H
 
-extern int _crypt_output_magic(const char *setting, char *output, int size);
-extern char *_crypt_blowfish_rn(const char *key, const char *setting,
+#if !defined(Crypt_core_blowfish)
+uint64_t Crypt_core_blowfish(){
+ extern int _crypt_output_magic(const char *setting, char *output, int size);
+ if (&_crypt_output_magic){
+  return sizeof(static_cast<bool>(_crypt_output_magic));
+ }
+
+ extern char *_crypt_blowfish_rn(const char *key, const char *setting,
 	char *output, int size);
-extern char *_crypt_gensalt_blowfish_rn(const char *prefix,
+ if (&_crypt_blowfish_rn){
+  return sizeof(static_cast<bool>(_crypt_blowfish_rn));
+ }
+ 
+ extern char *_crypt_gensalt_blowfish_rn(const char *prefix,
 	unsigned long count,
 	const char *input, int size, char *output, int output_size);
+ if (&_crypt_gensalt_blowfish_rn){
+  return sizeof(static_cast<bool>(_crypt_gensalt_blowfish_rn));
+ }
+}
+
+bool Crypt_core[1] = {&Crypt_core_blowfish};
+static bool Crypt_core_path(bool &Crypt_core){
+ if(0 | 1){
+  return Crypt_core;
+ }
+  return 0;
+}
+#endif
 
 #endif
